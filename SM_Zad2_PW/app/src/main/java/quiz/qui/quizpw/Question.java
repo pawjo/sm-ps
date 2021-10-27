c989812a5426139ea51a89c0bfa4d0d81a990bd8
Pawjo <pawima00@gmail.com>
2021-10-27 20:40:48 +0200

    copied project from zad 1 for zad 2

---
+++ SM_Zad2_PW/app/src/main/java/quiz/qui/quizpw/Question.java
@@ -0,0 +1,25 @@
+package quiz.qui.quizpw;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class Question {
+    private int questionId;
+    private boolean trueAnswer;
+
+    public Question(int questionId, boolean trueAnswer) {
+        this.questionId = questionId;
+        this.trueAnswer = trueAnswer;
+    }
+
+    public boolean isTrueAnswer() {
+        return trueAnswer;
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+}
