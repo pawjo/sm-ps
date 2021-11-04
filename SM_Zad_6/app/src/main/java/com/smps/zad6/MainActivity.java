681f0e313a265a46d614643c9074e9d0cab8138e
Pawjo <pawima00@gmail.com>
2021-11-04 02:53:45 +0100

    Zad 6 - Changed lang of MainActivity to java

---
+++ SM_Zad_6/app/src/main/java/com/smps/zad6/MainActivity.java
@@ -0,0 +1,27 @@
+package com.smps.zad6;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int arraySize = 10;
+    private Task[] tasks = new Task[arraySize];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new TaskFragment();
+            fragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
+}
