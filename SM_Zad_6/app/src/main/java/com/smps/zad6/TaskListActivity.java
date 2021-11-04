a919e1bd6e739ca9ef8a1812bee53f1ea89d9047
Pawjo <pawima00@gmail.com>
2021-11-04 03:03:17 +0100

    Zad 6 - Added TaskListActivity

---
+++ SM_Zad_6/app/src/main/java/com/smps/zad6/TaskListActivity.java
@@ -0,0 +1,14 @@
+package com.smps.zad6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaskListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
